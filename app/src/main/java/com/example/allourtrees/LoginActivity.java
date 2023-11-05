831cda04b3535cf023c2596b795295db6c89eef9
Maria <s185108@student.dtu.dk>
2023-11-05 18:08:37 +0100

    User creation and authentication works

--- app/src/main/java/com/example/allourtrees/LoginActivity.java
+++ app/src/main/java/com/example/allourtrees/LoginActivity.java
@@ -1,13 +1,21 @@
 package com.example.allourtrees;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,6 +23,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     TextView signUpInstead;
     Button signInBtn;
+
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +38,37 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         signInBtn = findViewById(R.id.signInBtn);
         signInBtn.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     @Override
     public void onClick(View view) {
         if(view == signInBtn){
+
+            String emailTrimmed = email.getText().toString().trim();
+            String passwordTrimmed = password.getText().toString().trim();
+
+            mAuth.signInWithEmailAndPassword(emailTrimmed, passwordTrimmed)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+
+                                Toast.makeText(LoginActivity.this, "Authentication success.",
+                                        Toast.LENGTH_SHORT).show();
+                                Log.d("MARIA", "signInWithEmail:success");
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w("MARIA", "signInWithEmail:failure", task.getException());
+                                Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
 
         }else if(view == signUpInstead){
             Intent mIntent = new Intent(this, CreateUserActivity.class);
