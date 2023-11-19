368298beaceb3c92f787e7e0c95939c57cd0d041
Maria <s185108@student.dtu.dk>
2023-11-19 15:43:09 +0100

    Added location permissions in manifest and distance calculator in attraction

--- app/src/main/java/com/example/allourtrees/Attraction.java
+++ app/src/main/java/com/example/allourtrees/Attraction.java
@@ -1,6 +1,12 @@
 package com.example.allourtrees;
 
+import android.content.Context;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
 public class Attraction {
+    private static final double EARTH_RADIUS = 6371;
     double longitude;
     double lattitude;
 
@@ -12,6 +18,8 @@ public class Attraction {
     String attractionCategory;
 
     int price; //ranged from 0-4??
+
+
 
 
     public Attraction(double longitude, double lattitude, String attractionName, String attractionDescriptionShort, String attractionDescriptionLong, int attractionDefaultPicture, String attractionCategory) {
@@ -87,7 +95,21 @@ public class Attraction {
         return(0.0);
     }
 
+    double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        double lat1Rad = Math.toRadians(lat1);
+        double lat2Rad = Math.toRadians(lat2);
+        double lon1Rad = Math.toRadians(lon1);
+        double lon2Rad = Math.toRadians(lon2);
+
+        double x = (lon2Rad - lon1Rad) * Math.cos((lat1Rad + lat2Rad) / 2);
+        double y = (lat2Rad - lat1Rad);
+        double distance = Math.sqrt(x * x + y * y) * EARTH_RADIUS;
+
+        return distance;
+    }
+
     public boolean hasBeenVisitedBefore(){
         return true;
     }
+
 }
