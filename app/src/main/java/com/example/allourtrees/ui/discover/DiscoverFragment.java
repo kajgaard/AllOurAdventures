3f75c826f6b2a9734ccb3d11acf88088ddda19c6
Maria <s185108@student.dtu.dk>
2023-11-25 15:35:58 +0100

    Map shows with test pin

--- app/src/main/java/com/example/allourtrees/ui/discover/DiscoverFragment.java
+++ app/src/main/java/com/example/allourtrees/ui/discover/DiscoverFragment.java
@@ -1,19 +1,38 @@
 package com.example.allourtrees.ui.discover;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import com.example.allourtrees.databinding.FragmentDiscoverBinding;
 
-public class DiscoverFragment extends Fragment {
+import com.example.allourtrees.R;
+import com.example.allourtrees.databinding.FragmentDiscoverBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class DiscoverFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentDiscoverBinding binding;
+
+    GoogleMap gMap;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,8 +42,10 @@ public class DiscoverFragment extends Fragment {
         binding = FragmentDiscoverBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDiscover;
-        discoverViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         return root;
     }
 
@@ -33,4 +54,48 @@ public class DiscoverFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+            this.gMap = googleMap;
+        LatLng dtuMarker = new LatLng(55.78243354245375, 12.513651004508711);
+        this.gMap.addMarker(new MarkerOptions().position(dtuMarker).title("TEST MARKER").icon(BitmapFromVector(getContext(),R.drawable.seen_pin)));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(dtuMarker));
+
+    }
+
+    private BitmapDescriptor
+    BitmapFromVector(Context context, int vectorResId)
+    {
+        // below line is use to generate a drawable.
+        Drawable vectorDrawable = ContextCompat.getDrawable(
+                context, vectorResId);
+
+        // below line is use to set bounds to our vector
+        // drawable.
+        vectorDrawable.setBounds(
+                0, 0, vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight());
+
+        // below line is use to create a bitmap for our
+        // drawable which we have added.
+        Bitmap bitmap = Bitmap.createBitmap(
+                vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(),
+                Bitmap.Config.ARGB_8888);
+
+        // below line is use to add bitmap in our canvas.
+        Canvas canvas = new Canvas(bitmap);
+
+        // below line is use to draw our
+        // vector drawable in canvas.
+        vectorDrawable.draw(canvas);
+
+        // after generating our bitmap we are returning our
+        // bitmap.
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+
+
 }
